70e0457cbeb2b35ffbcad21bd12109e6de5362a5
yongming <yongming@solarmonkey.nl>
2019-10-25 22:20:55 +0200

    Use testing tags for non-master branches

--- .github/actions/autotag/get_new_tag.py
+++ .github/actions/autotag/get_new_tag.py
@@ -1,8 +1,10 @@
 import datetime
+import os
 import sys
 
 
 def new_version(most_recent_version: str):
+    branch = os.environ.get("GITHUB_REF")
     current_year, current_week, _ = datetime.date.today().isocalendar()
     current_version = 0
 
@@ -13,8 +15,12 @@ def new_version(most_recent_version: str):
 
     if old_year == current_year and old_week == current_week:
         current_version = old_version + 1
+    new_tag = f"{current_year}.{current_week}.{current_version}"
+    
+    if branch and branch != "master":
+        new_tag = "test." + new_tag
 
-    print(f"{current_year}.{current_week}.{current_version}")
+    print(new_tag)
 
 
 if __name__ == "__main__":
